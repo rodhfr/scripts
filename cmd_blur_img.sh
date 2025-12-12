47304a3255a60d03e1fd1df41005b5543feb1a27
rodhfr <souzafrodolfo@gmail.com>
2025-12-12 15:44:04 -0300

    delete_old

--- cmd_blur_img.sh
+++
@@ -1,2 +0,0 @@
-magick input.jpeg   -resize 50%   -gaussian-blur 0x8   -resize 200%   output.jpg
-
--- cmd_blur_img_dessaturate.sh
+++
@@ -1,2 +0,0 @@
- magick input.jpeg   -resize 50%   -gaussian-blur 0x8   -resize 200%   -modulate 110,50,100   output.jpg
-
